07aa0f293e2902ea3987205b6a96e1d4b221f5c7
Chao216 <co.jiaan@proton.me>
2022-06-09 21:47:27 +0300

    <%- -%>

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 var items =["buy food", "cook food", "eat food"];
+var workItems=[];
 app.set("view engine", 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("publik"))
@@ -13,23 +14,43 @@ app.get("/", (req,res)=>{
   var today = new Date().getDay()
 
   var options = {weekday:"long", day:"numeric", month:"long"}
-  var day = new Date().toLocaleDateString("zh-CN", options);
+  var day = new Date().toLocaleDateString("en-US", options);
   res.render("list", {whatDay:day,itemArray:items});
 })
 
-app.post("/", (req,res)=>{
-  var item =req.body.newItem
-  items.push(item)
+app.get("/work", (req,res)=>{
+  res.render("list", {whatDay:"Work", itemArray:workItems})
+})
 
-  res.redirect("/")
+
+app.post("/", (req,res)=>{
+  console.log(req.body);
+
+  var item =req.body.newItem
+  if (req.body.button==="Work"){
+
+    workItems.push(item)
+    res.redirect("/work")
+
+  } else{
+    items.push(item)
+
+    res.redirect("/")
+
+  }
+
+
 
 
 })
 
+app.get("/about", (req,res)=>{
+  res.render("about")
+})
 
-
-
-
+app.get("/story", (req,res)=>{
+  res.render("story")
+})
 
 
 app.listen(port, (req,res)=>{
